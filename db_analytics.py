cda2ca2bb3d6ebe3b0362719b3f6fee2c2c9da77
Jake Convery <jakeconv@gmail.com>
2019-08-06 19:38:22 -0400

    Updates to conform to PEP8 style

--- db_analytics.py
+++ db_analytics.py
@@ -7,7 +7,6 @@ import psycopg2
 
 def main():
 
-
     # First, open the database connection.  Throw an error if it does not work.
     try:
         db = psycopg2.connect("dbname=news")
@@ -17,11 +16,11 @@ def main():
         print(e.diag.message_detail)
         sys.exit(1)
 
-    # Part 1: the three most popular articles of all time    
+    # Part 1: the three most popular articles of all time
     c = db.cursor()
-    c.execute("select b.title, count(a.path) as num from log as a, "
-              + "articles as b where a.path like concat('/article/', b.slug) "
-              + "group by b.title order by num desc limit 3;")
+    c.execute("""select b.title, count(a.path) as num from log as a,
+              articles as b where a.path like concat('/article/', b.slug)
+              group by b.title order by num desc limit 3;""")
     results = c.fetchall()
     print("Top 3 Articles: ")
     print("\"" + results[0][0] + "\" -- " + str(results[0][1]) + " views")
@@ -29,22 +28,22 @@ def main():
     print("\"" + results[2][0] + "\" -- " + str(results[2][1]) + " views")
 
     # Part 2: the top authors of the publication
-    c.execute("select name, sum(hits) as views from articleHits group by name "
-              + "order by views desc;")
+    c.execute("""select name, sum(hits) as views from articleHits group by name
+              order by views desc;""")
     results = c.fetchall()
     print("\nTop authors in database: ")
     for x in range(0, len(results)):
         print(results[x][0] + " -- " + str(results[x][1]) + " views")
 
     # Part 3: days where error rate was greater than 1%
-    c.execute("select date, (bad::float/total)*100 as pct from requests where "
-              + "(bad::float/total)*100 > 1;")
+    c.execute("""select date, (bad::float/total)*100 as pct from requests where
+              (bad::float/total)*100 > 1;""")
     results = c.fetchall()
     db.close()
     print("\nDays where more than 1% of requests led to errors:")
     for x in range(0, len(results)):
-        print(results[x][0].strftime("%B %d, %Y") + " -- " + str(results[x][1])
-              + "% errors")
+        print(results[x][0].strftime("%B %d, %Y") +
+              " -- " + str(results[x][1]) + "% errors")
 
 
 if __name__ == '__main__':
